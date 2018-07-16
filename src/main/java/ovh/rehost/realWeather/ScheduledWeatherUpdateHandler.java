cb5a94b4fecbaa8898c7fe4895fce463341a8d1f
Michał Kubik <bopke2@gmail.com>
2018-07-17 00:49:31 +0200

    Add thunderstorms support

--- src/main/java/ovh/rehost/realWeather/ScheduledWeatherUpdateHandler.java
+++ src/main/java/ovh/rehost/realWeather/ScheduledWeatherUpdateHandler.java
@@ -14,15 +14,23 @@ public class ScheduledWeatherUpdateHandler implements Runnable {
 
     @Override
     public void run() {
-        if (weatherID < 700) {
+        if (weatherID < 300) {
+            RealWeather.setCurrentWeather(RealWeather.WeatherStates.THUNDERSTORM);
+            for (String i : RealWeather.getAffectedWorlds()) {
+                plugin.getServer().getWorld(i).setStorm(true);
+                plugin.getServer().getWorld(i).setThundering(true);
+            }
+        } else if (weatherID < 700) {
             RealWeather.setCurrentWeather(RealWeather.WeatherStates.RAIN);
             for (String i : RealWeather.getAffectedWorlds()) {
                 plugin.getServer().getWorld(i).setStorm(true);
+                plugin.getServer().getWorld(i).setThundering(false);
             }
         } else {
             RealWeather.setCurrentWeather(RealWeather.WeatherStates.CLEAR);
             for (String i : RealWeather.getAffectedWorlds()) {
                 plugin.getServer().getWorld(i).setStorm(false);
+                plugin.getServer().getWorld(i).setThundering(false);
             }
         }
     }
